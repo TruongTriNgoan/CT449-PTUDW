f5feb9f965776b1ee81b7e21a8225111e68d3f5e
B2014590_Trương Trí Ngoan <ngoanb2014590@student.ctu.edu.vn>
2024-04-24 21:20:56 +0700

    update

--- app/routers/contact.route.js
+++ app/routers/contact.route.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const contacts = require("../controllers/contact.controller");
 
-const router = express.Router();
+const router =  express.Router();
 
 router.route("/")
     .get(contacts.findAll)
     .post(contacts.create)
     .delete(contacts.deleteAll);
 
-router.route("/favorite")
-    .get(contacts.findAllFavorite);
+router.route("/favorites")
+    .get(contacts.findAllFavorites);
 
 router.route("/:id")
     .get(contacts.findOne)
--- server.js
+++ server.js
@@ -2,17 +2,19 @@ const app = require("./app");
 const config = require("./app/config");
 const MongoDB = require("./app/utils/mongodb.util");
 
-// start server
 async function startServer() {
-  try {
-    await MongoDB.connect(config.db.uri);
-    console.log("Connect to the database!");
+    try{
+        await MongoDB.connect(config.db.uri);
+        console.log("Connected to the database!");
 
-    const PORT = config.app.port;
-    app.listen(PORT, () => {
-      console.log(`Server is running on ${PORT}`);
-    });
-  } catch (error) {
-    console.log("Cannot connect to the database!", error);
-  }
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch(error){
+        console.log("Cannot connect to the database!", error);
+        process.exit();
+    }
 }
+
+startServer();
